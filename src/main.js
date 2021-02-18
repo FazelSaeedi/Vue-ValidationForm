325c8b14f12ff95fd5ce4a5fc98af36c7b100244
FazelSaeedi <FazelSaeedi@iran.ir>
2021-02-18 06:07:20 +0330

    initialize Vuelidate && bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from "vuelidate";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App),
